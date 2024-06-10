7b973c4c603587799d8a157758e4b5a6e3f8ef92
Keegan Erickson <kmerickson@protonmail.com>
2024-06-10 15:45:38 -0600

    Adding file for homework assignment for section 5.14 problem 10.

---
+++ week3_ch4_ch5/sec5_14prob10.py
@@ -0,0 +1,35 @@
+# name: sec5_14prob10.property
+# Assume the days of the week are numbered 0,1,2,3,4,5,6 from 
+# Sunday to Saturday. This script shows  a function which is 
+# given the day number, and it returns the day name (a string).
+# http://openbookproject.net/thinkcs/python/english3e/functions.html
+# author: Keegan Erickson
+# date: June 10, 2024
+
+
+def dayOfWeek(n):
+    if n == 0:
+        d = "Sunday"
+    elif n == 1:
+        d = "Monday"
+    elif n == 2:
+        d = "Tuesday"
+    elif n == 3:
+        d = "Wednesday"
+    elif n == 4:
+        d = "Thursday"
+    elif n == 5:
+        d = "Friday"
+    elif n == 6:
+        d = "Saturday"
+    else:
+        d = "That is not a valid entry."
+
+    return d
+
+n = input("Please type in a number, 0-6, to represent a day of the week: ")
+n = int(n)
+
+s = dayOfWeek(n)
+print(s)
+
